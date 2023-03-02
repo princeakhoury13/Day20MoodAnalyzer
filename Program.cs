81fac8fb23972e5fd70a5e4b75ebbb092a010b45
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 18:28:23 +0530

    added: analyze mood function

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Day20MoodAnalyzer
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day20MoodAnalyzer
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch for assignment: Day 20 Mood Analyzer");
+            string message = "I am happy";
+
+            AnalyzeMood(message);
+            
+        }
+
+        public static void AnalyzeMood(string message)
+        {
+            string happy = "I am happy";
+            string sad = "I am sad";
+
+            if(message == happy)
+            {
+                Console.WriteLine("Mood: Happy");
+            }
+            else if(message == sad)
+            {
+                Console.WriteLine("Mood: Sad");
+            }
         }
     }
 }
