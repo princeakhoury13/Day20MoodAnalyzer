f06b3d7e1ac4a0d306b0b5469b45c2a6a0e2b8dc
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 18:50:11 +0530

    added: handle null message exception

---
+++ AnalyzeMood.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MoodAnalyzer
+{
+    public class AnalyzeMood
+    {
+        public string message;
+        public string happy = "I am happy";
+        public string sad = "I am sad";
+      
+
+        public AnalyzeMood(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ArgumentException("No message was passed");
+            }
+            this.message = message;
+        }
+
+        public void Analyze()
+        {
+            if(this.message == happy)
+            {
+                Console.WriteLine("Mood: Happy");
+            }
+            else if(this.message == sad)
+            {
+                Console.WriteLine("Mood: Sad");
+            }
+        }
+
+    }
+}
