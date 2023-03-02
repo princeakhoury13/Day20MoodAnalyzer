a68c65a3910f07a36c24539944976e7a0adc1898
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 18:23:04 +0530

    added: Master branch greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day20MoodAnalyzer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the master branch for assignment: Day 20 Mood Analyzer");
+        }
+    }
+}
