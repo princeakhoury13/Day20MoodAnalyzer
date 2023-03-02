635101b906cd1b58f7c21dbb8b007b44b22b1070
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 18:57:26 +0530

    added: created custom exception to handle null message

---
+++ MoodAnalysisException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MoodAnalyzer
+{
+    public class MoodAnalysisException: Exception
+    {
+        public MoodAnalysisException(string message) : base(message)
+        {
+
+        }
+    }
+}
